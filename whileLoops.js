acc8d44bea0f88a9132c1fe2ee8f3431be142ccb
Adam Joseph <amjoseph93@gmail.com>
2017-01-14 16:42:42 -0800

    First commit: project unfinished

---
+++ whileLoops.js
@@ -0,0 +1,19 @@
+//Filter Dollars out of remainder and set inner text to value
+var dollars = 0;
+while(change >= 1) {
+	change -= 1;
+	dollars += 1;
+};
+
+//Filter Quarters out of remainder and set inner text to value
+var quarters = 0;
+while(0 <= change <= 0.99) {
+change % .25
+
+};
+
+//Filter Dimes out of remainder and set inner text to value
+var dimes = 0;
+
+//Filter Pennies out of remainder and set inner text to value
+var pennies = 0;
